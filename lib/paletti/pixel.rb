2af6fa9b0b55a939da43f3db62f9901642e59274
Satyam Ghodasara <sghodas@gmail.com>
2015-11-24 22:46:17 -0500

    Added is_distinct? method to Magick::Pixel

--- lib/paletti/pixel.rb
+++ lib/paletti/pixel.rb
@@ -7,4 +7,20 @@ class Magick::Pixel
     (r > white_thresh && g > white_thresh && b > white_thresh) || (r < black_thresh && g < black_thresh && b < black_thresh) ? true : false
   end
 
+  def is_distinct?(other_pixel)
+    # Calculate contrast using the W3C formula
+    #   http://www.w3.org/TR/WCAG20-TECHS/G145.html
+    lum = 0.2126 * this.red + 0.7152 * this.green + 0.0722 * this.blue
+    other_pixel_lum = 0.2126 * other_pixel.red + 0.7152 * other_pixel.green + 0.0722 * other_pixel.blue
+
+    contrast = 0
+    if lum > other_pixel_lum
+      contrast = (lum.to_f + 0.05) / (other_pixel_lum.to_f + 0.05)
+    else
+      contrast = (other_pixel_lum.to_f + 0.05) / (lum.to_f + 0.05)
+    end
+
+    return contrast.to_f > 3.0
+  end
+
 end
--- paletti.gemspec
+++ paletti.gemspec
@@ -6,7 +6,7 @@ Gem::Specification.new do |s|
   s.description = "Paletti takes an image and finds its background color as well as the best primary, secondary, and detail text colors that are readable on the background color."
   s.author      = "Satyam Ghodasara"
   s.email       = 'sghodas@gmail.com'
-  s.files       = ["lib/paletti.rb"]
+  s.files       = ["lib/paletti.rb", "lib/paletti/pixel.rb"]
   s.homepage    = 'https://github.com/sghodas/paletti'
   s.license     = 'MIT'
 end
